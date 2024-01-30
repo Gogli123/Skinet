dcaa81306151f35dea172b7043d5ec33059eab0c
unknown <sandro.goglidze.1@btu.edu.ge>
2024-01-30 22:15:39 +0400

    Section 14

--- client/src/app/core/nav-bar/nav-bar.component.ts
+++ client/src/app/core/nav-bar/nav-bar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BasketService } from '../../basket/basket.service';
+import { Observable } from 'rxjs';
+import { IBasket } from '../../shared/models/basket';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,8 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent implements OnInit{
+  basket$: Observable<IBasket>;
+
+  constructor(private basketService: BasketService) { }
+
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.basket$ = this.basketService.basket$;
   }
 
 }
